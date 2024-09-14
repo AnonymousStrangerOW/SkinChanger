9ae2c4067b1ddf4c8a8f9c776945d4ba8d8c6a3b
JohnCorby <williamforgames6@gmail.com>
2024-09-14 12:28:51 -0700

    rewrite half the mod (async bundle loading, stop using hack for hatchling)

--- SkinChanger/SkinChanger.cs
+++ SkinChanger/SkinChanger.cs
@@ -1,206 +1,202 @@
-﻿using Epic.OnlineServices;
-using OWML.Common;
+﻿using OWML.Common;
 using OWML.ModHelper;
-using OWML.ModHelper.Events;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
-using UnityEngine.Assertions;
 
 namespace SkinChanger
 {
-    public class SkinChanger : ModBehaviour
-    {
-        List<PlayableCharacter> characters;
+	public class SkinChanger : ModBehaviour
+	{
+		public static SkinChanger instance;
 
-        public GameObject playerNude;
-        public GameObject playerClothed;
+		public PlayerCameraController PlayerCamera;
+		public CapsuleCollider PlayerCollider;
+		public GameObject Traveller_HEA_Player_v2;
 
-        public PlayerCameraController CamOffset;
-        public CapsuleCollider PlayerCollider;
-        public bool IsPlayerSuited;
-        const string Hatchling = "Hatchling";
+		Dictionary<string, GameObject> prefabs = new();
+		List<PlayableCharacter> characters;
 
-        public static SkinChanger instance;
+		class PlayableCharacter
+		{
+			public GameObject GameObject;
+			public string SettingName;
+			public Vector3 CameraOffset;
 
-        public static AssetBundle skin;
+			public float ColliderRadius;
+			public float ColliderHeight;
+			public Vector3 ColliderCenter;
 
-        public Dictionary<string, GameObject> skinObjects;
+			public PlayableCharacter(string prefabName, string settingName, Vector3 camOffset, float collRadius, float collHeight, Vector3 collCenter)
+			{
+				GameObject = prefabName != null ? Instantiate(instance.prefabs[prefabName]) : instance.Traveller_HEA_Player_v2;
+				SettingName = settingName;
+				CameraOffset = camOffset;
 
-        class PlayableCharacter
-        {
-            public string name;
+				ColliderRadius = collRadius;
+				ColliderHeight = collHeight;
+				ColliderCenter = collCenter;
+			}
+		}
 
-            public GameObject gameObject;
-            public string SettingName;
-            public Vector3 CameraOffset;
+		// stolen from qsb and nh LOLOLLOLOLOLO
+		public static void ReplaceShaders(GameObject prefab)
+		{
+			foreach (var renderer in prefab.GetComponentsInChildren<Renderer>(true))
+			{
+				foreach (var material in renderer.sharedMaterials)
+				{
+					if (material == null) continue;
 
-            public float ColliderRadius;
-            public float ColliderHeight;
-            public Vector3 ColliderCenter;
+					var replacementShader = Shader.Find(material.shader.name);
+					if (replacementShader == null) continue;
 
+					// preserve override tag and render queue (for Standard shader)
+					// keywords and properties are already preserved
+					if (material.renderQueue != material.shader.renderQueue)
+					{
+						var renderType = material.GetTag("RenderType", false);
+						var renderQueue = material.renderQueue;
+						material.shader = replacementShader;
+						material.SetOverrideTag("RenderType", renderType);
+						material.renderQueue = renderQueue;
+					}
+					else
+					{
+						material.shader = replacementShader;
+					}
+				}
+			}
+		}
 
+		private void Start()
+		{
+			instance = this;
 
-            public PlayableCharacter(string name, string settingName, Vector3 camOffset, float collRadius, float collHeight, Vector3 collCenter)
-            {
-                this.name = name;
-                gameObject = GameObject.Instantiate(skin.LoadAsset<GameObject>(name+".prefab"));
+			foreach (var path in Directory.EnumerateFiles(Path.Combine(ModHelper.Manifest.ModFolderPath, "Assets")))
+			{
+				if (Path.GetExtension(path) == ".manifest") continue; // ignore the non bundle files
 
-                if (gameObject == null)
-                {
-                    instance.ModHelper.Console.WriteLine(name + " JKLLLLLLLLLLLLLLLLLLLLLLL,DSDK/.CGBJKHG,BUHASO,L.BFGN,LUSNV,CCCCCCBVGLRFEWIHSD");
-                }
-                SettingName = settingName;
-                CameraOffset = camOffset;
-                ColliderRadius = collRadius;
-                ColliderHeight = collHeight;
-                ColliderCenter = collCenter;
-            }
-        }
+				ModHelper.Console.WriteLine($"loading async {path}");
+				var req1 = AssetBundle.LoadFromFileAsync(path);
+				req1.completed += _ =>
+				{
+					var req2 = req1.assetBundle.LoadAllAssetsAsync<GameObject>();
+					req2.completed += _ =>
+					{
+						var prefab = (GameObject)req2.asset;
+						ReplaceShaders(prefab);
+						prefabs.Add(prefab.name, prefab);
+						ModHelper.Console.WriteLine($"loaded prefab {prefab}");
+					};
+				};
+			}
 
-        public interface IQSBAPI
-        {
+			LoadManager.OnCompleteSceneLoad += (scene, loadScene) =>
+			{
+				if (loadScene is not (OWScene.SolarSystem or OWScene.EyeOfTheUniverse)) return;
 
-        }
+				var player = GameObject.Find("Player_Body");
+				PlayerCamera = player.transform.Find("PlayerCamera").GetComponent<PlayerCameraController>();
 
-        private void Start()
-        {
-            instance = this;
+				PlayerCollider = player.GetComponent<CapsuleCollider>();
 
-            skin = ModHelper.Assets.LoadBundle("planets/Assets/skin");
+				Traveller_HEA_Player_v2 = player.transform.Find("Traveller_HEA_Player_v2").gameObject;
 
-            // Starting here, you'll have access to OWML's mod helper.
-            ModHelper.Console.WriteLine("Other Playable Characters is loaded!", MessageType.Success);
+				// make the instances of the prefabs
+				characters = new List<PlayableCharacter>()
+				{
+					//Unity Object name                                         //Setting Name      //Camera Offset                     //Collider Radius, Height, Center
+					new PlayableCharacter(null /*uses existing v2*/, "Hatchling", new Vector3(0, 0.8496093f, 0.15f), 0.5f, 2f, Vector3.zero),
+					new PlayableCharacter("Traveller_HEA_Player_v3", "Inhabitant", new Vector3(0, 2.2f, 0.27f), 0.5f, 3.6f, new Vector3(0f, 0.875f, 0f)),
+					new PlayableCharacter("Traveller_HEA_Player_v4", "Nomai", new Vector3(0, 1.1f, 0.3f), 0.5f, 2.5f, new Vector3(0f, 0.3f, 0f)),
+					new PlayableCharacter("Traveller_HEA_Player_v5", "Chert", new Vector3(0, 0.3f, 0.2f), 0.5f, 1.5f, new Vector3(0f, -0.2f, 0f)),
+					new PlayableCharacter("Traveller_HEA_Player_v6", "Esker", new Vector3(0, 0.9f, 0.2f), 0.5f, 2f, Vector3.zero),
+					new PlayableCharacter("Traveller_HEA_Player_v7", "Riebeck", new Vector3(0, 1.1f, 0.3f), 0.5f, 2.5f, new Vector3(0f, 0.2f, 0f)),
+					new PlayableCharacter("Traveller_HEA_Player_v8", "Gabbro", new Vector3(0, 1.1f, 0.2f), 0.5f, 2.5f, new Vector3(0f, 0.2f, 0)),
+					new PlayableCharacter("Traveller_HEA_Player_v9", "Feldspar", new Vector3(0f, 0.4f, 0.2f), 0.5f, 2f, Vector3.zero),
+					new PlayableCharacter("Traveller_HEA_Player_v10", "Slate", new Vector3(0, 1.2f, 0.2f), 0.5f, 2.5f, new Vector3(0f, 0.2f, 0)),
+					new PlayableCharacter("Traveller_HEA_Player_v11", "Hal", new Vector3(0, 0.8496093f, 0.15f), 0.5f, 2f, Vector3.zero),
+					new PlayableCharacter("Traveller_HEA_Player_v12", "Hornfels", new Vector3(0, 1.2f, 0.2f), 0.5f, 2.5f, new Vector3(0f, 0.2f, 0)),
+					new PlayableCharacter("Traveller_HEA_Player_v13", "Gossan", new Vector3(0f, 0.5f, 0.1f), 0.5f, 2f, Vector3.zero),
+					new PlayableCharacter("Traveller_HEA_Player_v14", "Mica", new Vector3(0, 0.3f, 0.1f), 0.5f, 1.5f, new Vector3(0f, -0.2f, 0)),
+					new PlayableCharacter("Traveller_HEA_Player_v15", "Arkose", new Vector3(0, 0.3f, 0.1f), 0.5f, 1.5f, new Vector3(0f, -0.2f, 0)),
+					new PlayableCharacter("Traveller_HEA_Player_v16", "Tephra", new Vector3(0, 0.3f, 0.1f), 0.5f, 1.5f, new Vector3(0f, -0.2f, 0)),
+					new PlayableCharacter("Traveller_HEA_Player_v17", "Galena", new Vector3(0, 0.3f, 0.1f), 0.5f, 1.5f, new Vector3(0f, -0.2f, 0)),
+					new PlayableCharacter("Traveller_HEA_Player_v18", "Spinel", new Vector3(0, 0.7f, 0.2f), 0.5f, 2f, Vector3.zero),
+					new PlayableCharacter("Traveller_HEA_Player_v19", "Porphy", new Vector3(0, 1.3f, 0.2f), 0.5f, 2.5f, new Vector3(0f, 0.25f, 0)),
+					new PlayableCharacter("Traveller_HEA_Player_v20", "Rutile", new Vector3(0, 0.8496093f, 0.15f), 0.5f, 2f, Vector3.zero),
+					new PlayableCharacter("Traveller_HEA_Player_v21", "Marl", new Vector3(0, 1.3f, 0.25f), 0.5f, 2.5f, new Vector3(0f, 0.25f, 0)),
+					new PlayableCharacter("Traveller_HEA_Player_v22", "Gneiss", new Vector3(0f, 0.4f, 0.2f), 0.5f, 2f, Vector3.zero),
+					new PlayableCharacter("Traveller_HEA_Player_v23", "Moraine", new Vector3(0, 0.3f, 0.1f), 0.5f, 1.5f, new Vector3(0f, -0.2f, 0)),
+					new PlayableCharacter("Traveller_HEA_Player_v24", "Tuff", new Vector3(0, 0.8496093f, 0.15f), 0.5f, 2f, Vector3.zero),
+					new PlayableCharacter("Traveller_HEA_Player_v25", "Tektite", new Vector3(0, 1.2f, 0.2f), 0.5f, 2.5f, new Vector3(0f, 0.25f, 0))
+				};
 
-            // Get the New Horizons API and load configs
-            var newHorizons = ModHelper.Interaction.TryGetModApi<INewHorizons>("xen.NewHorizons");
-            newHorizons.LoadConfigs(this);
+				// parent them to the player
+				foreach (var character in characters)
+				{
+					character.GameObject.transform.SetParent(player.transform, false);
+					// this terrible position comes from vanilla player. cry about it
+					character.GameObject.transform.localPosition = new Vector3(0, -1.03f, -0.2f);
+					character.GameObject.transform.localScale = Vector3.one * .1f;
+				}
 
-            {
-                GlobalMessenger.AddListener("SuitUp", () =>
-                {
-                    IsPlayerSuited = true;
-                    ModHelper.Console.WriteLine(IsPlayerSuited);
-                });
+				ChangeSkin();
+			};
+		}
 
-                GlobalMessenger.AddListener("RemoveSuit", () =>
-                {
-                    IsPlayerSuited = false;
-                    ModHelper.Console.WriteLine(IsPlayerSuited);
-                });
-            }
+		public override void Configure(IModConfig config)
+		{
+			if (LoadManager.s_currentScene is not (OWScene.SolarSystem or OWScene.EyeOfTheUniverse)) return;
+			ChangeSkin();
+		}
 
-            // Example of accessing game code.
-            LoadManager.OnCompleteSceneLoad += (scene, loadScene) =>
-            {
-                if (loadScene != OWScene.SolarSystem) return;
-                ModHelper.Console.WriteLine("Loaded into solar system!", MessageType.Success);
-            };
+		const string ModelSetting = "Model";
+		const string ChangeCamera = "Change Camera";
+		const string ChangeCollider = "Change Collider";
 
-            newHorizons.GetStarSystemLoadedEvent().AddListener((system) =>
-            {
-                var Cam = GameObject.Find("PlayerCamera");
-                CamOffset = Cam.GetComponent<PlayerCameraController>();
+		void ChangeSkin()
+		{
+			var modelSetting = ModHelper.Config.GetSettingsValue<string>(ModelSetting);
+			var useCamera = ModHelper.Config.GetSettingsValue<bool>(ChangeCamera);
+			var useCollider = ModHelper.Config.GetSettingsValue<bool>(ChangeCollider);
 
-                playerNude = GameObject.Find("Traveller_HEA_Player_v0");
-                playerClothed = GameObject.Find("Traveller_HEA_Player_v1");
-                PlayerCollider = GameObject.Find("Player_Body").GetComponent<CapsuleCollider>();
+			if (!useCamera)
+			{
+				PlayerCamera._origLocalPosition = characters[0].CameraOffset;
+			}
 
-                characters = new List<PlayableCharacter>()
-                {   //Unity Object name                                         //Setting Name      //Camera Offset                     //Collider Radius, Height, Center
-                    new PlayableCharacter("Traveller_HEA_Player_v0",     Hatchling,          new Vector3(0, 0.8496093f, 0.15f),  0.5f, 2f,   Vector3.zero),
-                    new PlayableCharacter("Traveller_HEA_Player_v1",     Hatchling,          new Vector3(0, 0.8496093f, 0.15f),  0.5f, 2f,   Vector3.zero),
-                    new PlayableCharacter("Traveller_HEA_Player_v3",     "Inhabitant",       new Vector3(0, 2.2f, 0.27f),        0.5f, 3.6f, new Vector3(0f, 0.875f, 0f)),
-                    new PlayableCharacter("Traveller_HEA_Player_v4",     "Nomai",            new Vector3(0, 1.1f, 0.3f),         0.5f, 2.5f, new Vector3(0f, 0.3f, 0f)),
-                    new PlayableCharacter("Traveller_HEA_Player_v5",     "Chert",            new Vector3(0, 0.3f, 0.2f),         0.5f, 1.5f, new Vector3(0f, -0.2f, 0f)),
-                    new PlayableCharacter("Traveller_HEA_Player_v6",     "Esker",            new Vector3(0, 0.9f, 0.2f),         0.5f, 2f,   Vector3.zero),
-                    new PlayableCharacter("Traveller_HEA_Player_v7",     "Riebeck",          new Vector3(0, 1.1f, 0.3f),         0.5f, 2.5f, new Vector3(0f, 0.2f, 0f)),
-                    new PlayableCharacter("Traveller_HEA_Player_v8",     "Gabbro",           new Vector3(0, 1.1f, 0.2f),         0.5f, 2.5f, new Vector3(0f, 0.2f, 0)),
-                    new PlayableCharacter("Traveller_HEA_Player_v9",     "Feldspar",         new Vector3(0f, 0.4f, 0.2f),        0.5f, 2f,   Vector3.zero),
-                    new PlayableCharacter("Traveller_HEA_Player_v10",    "Slate",            new Vector3(0, 1.2f, 0.2f),         0.5f, 2.5f, new Vector3(0f, 0.2f, 0)),
-                    new PlayableCharacter("Traveller_HEA_Player_v11",    "Hal",              new Vector3(0, 0.8496093f, 0.15f),  0.5f, 2f,   Vector3.zero),
-                    new PlayableCharacter("Traveller_HEA_Player_v12",    "Hornfels",         new Vector3(0, 1.2f, 0.2f),         0.5f, 2.5f, new Vector3(0f, 0.2f, 0)),
-                    new PlayableCharacter("Traveller_HEA_Player_v13",    "Gossan",           new Vector3(0f, 0.5f, 0.1f),        0.5f, 2f,   Vector3.zero),
-                    new PlayableCharacter("Traveller_HEA_Player_v14",    "Mica",             new Vector3(0, 0.3f, 0.1f),         0.5f, 1.5f, new Vector3(0f, -0.2f, 0)),
-                    new PlayableCharacter("Traveller_HEA_Player_v15",    "Arkose",           new Vector3(0, 0.3f, 0.1f),         0.5f, 1.5f, new Vector3(0f, -0.2f, 0)),
-                    new PlayableCharacter("Traveller_HEA_Player_v16",    "Tephra",           new Vector3(0, 0.3f, 0.1f),         0.5f, 1.5f, new Vector3(0f, -0.2f, 0)),
-                    new PlayableCharacter("Traveller_HEA_Player_v17",    "Galena",           new Vector3(0, 0.3f, 0.1f),         0.5f, 1.5f, new Vector3(0f, -0.2f, 0)),
-                    new PlayableCharacter("Traveller_HEA_Player_v18",    "Spinel",           new Vector3(0, 0.7f, 0.2f),         0.5f, 2f,   Vector3.zero),
-                    new PlayableCharacter("Traveller_HEA_Player_v19",    "Porphy",           new Vector3(0, 1.3f, 0.2f),         0.5f, 2.5f, new Vector3(0f, 0.25f, 0)),
-                    new PlayableCharacter("Traveller_HEA_Player_v20",    "Rutile",           new Vector3(0, 0.8496093f, 0.15f),  0.5f, 2f,   Vector3.zero),
-                    new PlayableCharacter("Traveller_HEA_Player_v21",    "Marl",             new Vector3(0, 1.3f, 0.25f),        0.5f, 2.5f, new Vector3(0f, 0.25f, 0)),
-                    new PlayableCharacter("Traveller_HEA_Player_v22",    "Gneiss",           new Vector3(0f, 0.4f, 0.2f),        0.5f, 2f,   Vector3.zero),
-                    new PlayableCharacter("Traveller_HEA_Player_v23",    "Moraine",          new Vector3(0, 0.3f, 0.1f),         0.5f, 1.5f, new Vector3(0f, -0.2f, 0)),
-                    new PlayableCharacter("Traveller_HEA_Player_v24",    "Tuff",             new Vector3(0, 0.8496093f, 0.15f),  0.5f, 2f,   Vector3.zero),
-                    new PlayableCharacter("Traveller_HEA_Player_v25",    "Tektite",          new Vector3(0, 1.2f, 0.2f),         0.5f, 2.5f, new Vector3(0f, 0.25f, 0))
+			if (!useCollider)
+			{
+				PlayerCollider.radius = characters[0].ColliderRadius;
+				PlayerCollider.height = characters[0].ColliderHeight;
+				PlayerCollider.center = characters[0].ColliderCenter;
+			}
 
-                };
+			foreach (var character in characters)
+			{
+				if (modelSetting == character.SettingName)
+				{
+					character.GameObject.SetActive(true);
 
-                ChangeSkin();
+					if (useCamera)
+					{
+						PlayerCamera._origLocalPosition = character.CameraOffset;
+					}
 
-                skinObjects = new();
-
-                foreach (PlayableCharacter character in characters)
-                {
-                    skinObjects.Add(character.SettingName, GameObject.Instantiate(skin.LoadAsset<GameObject>("Assets/" + character.name + ".prefab"), PlayerCollider.transform));
-                }
-            });
-        }
-        public override void Configure(IModConfig config)
-        {
-            if (LoadManager.s_currentScene != OWScene.SolarSystem && LoadManager.s_currentScene != OWScene.EyeOfTheUniverse) return;
-            ChangeSkin();
-        }
-
-        const string ModelSetting = "Model";
-        const string ChangeCollider = "Change Collider";
-        const string ChangeCamera = "Change Camera";
-        void ChangeSkin()
-        {
-            var modelSetting = ModHelper.Config.GetSettingsValue<string>(ModelSetting);
-            var useCollider = ModHelper.Config.GetSettingsValue<bool>(ChangeCollider);
-            var useCamera = ModHelper.Config.GetSettingsValue<bool>(ChangeCamera);
-
-            if (!useCollider)
-            {
-                PlayerCollider.radius = characters[0].ColliderRadius;
-                PlayerCollider.height = characters[0].ColliderHeight;
-                PlayerCollider.center = characters[0].ColliderCenter;
-            }
-
-            else if (!useCamera)
-            {
-                CamOffset._origLocalPosition = characters[0].CameraOffset;
-            }
-
-            foreach (var character in characters)
-            {
-                if (modelSetting == character.SettingName)
-                {
-                    character.gameObject.SetActive(true);
-                    if (useCamera) 
-                    {
-                        CamOffset._origLocalPosition = character.CameraOffset;
-                    }
-
-                    if (useCollider) {
-                        PlayerCollider.radius = character.ColliderRadius;
-                        PlayerCollider.height = character.ColliderHeight;
-                        PlayerCollider.center = character.ColliderCenter;
-                    }
-                }
-                else {
-                    character.gameObject.SetActive(false);
-                }
-            }
-        }
-        void Update() //Force player active
-        {
-            var modelSetting = ModHelper.Config.GetSettingsValue<string>(ModelSetting);
-            if (modelSetting == Hatchling)
-            {
-                playerNude.SetActive(!IsPlayerSuited);
-                playerClothed.SetActive(IsPlayerSuited);
-            }
-        }
-    }
+					if (useCollider)
+					{
+						PlayerCollider.radius = character.ColliderRadius;
+						PlayerCollider.height = character.ColliderHeight;
+						PlayerCollider.center = character.ColliderCenter;
+					}
+				}
+				else
+				{
+					character.GameObject.SetActive(false);
+				}
+			}
+		}
+	}
 }
